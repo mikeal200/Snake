acdfe28813806285f37ca6125a193ef5a115b120
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-12-16 17:30:00 -0500

    gui now appears and keys mapped

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,1 +1,49 @@
-public class Game
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener {
+    static final int SCREEN_WIDTH = 800;
+    static final int SCREEN_HEIGHT = 600;
+    char direction = 'R';
+
+    GamePanel() {
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new Keys());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    public class Keys extends KeyAdapter { 
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch(e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public class public static void main(String[] args) {
+    public static void main(String[] args) {
         new GameFrame();       
     }
 }
