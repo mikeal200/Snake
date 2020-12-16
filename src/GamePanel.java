537d62aed25147df8f7cc86af5707716ee3d1086
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-12-16 17:50:36 -0500

    delay changed to 60 increasing framerate

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -8,7 +8,7 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 20;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
-	static final int DELAY = 175;
+	static final int DELAY = 60;
 	final int x[] = new int[GAME_UNITS];
 	final int y[] = new int[GAME_UNITS];
     int snakeSize = 6;
