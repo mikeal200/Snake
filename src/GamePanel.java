5bfa6a5ae4debf17f66843681edd3b6fe9ba18d3
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-12-16 17:39:48 -0500

    snake appears on screen

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,13 +6,41 @@ import java.util.Random;
 public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 800;
     static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 20;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
+	static final int DELAY = 175;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+    int snakeSize = 6;
+    boolean running = true;
     char direction = 'R';
+    int appleX;
+    int appleY;
+    Timer timer;
+    Random random;
 
     GamePanel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
         this.addKeyListener(new Keys());
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        if(running) {
+            for(int i = 0; i < snakeSize; i++) {
+                if(i == 0) {
+                    g.setColor(Color.GREEN);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+        }
     }
 
     @Override
